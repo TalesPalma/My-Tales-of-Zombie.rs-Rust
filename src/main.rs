65a290a4cd152d96b7d8ac0393dca62dcdb248f9
TalesPalma <tales.palma_2022@outlook.com>
2024-06-04 20:38:09 -0300

    att project

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,28 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup,setup)
+        .add_systems(Startup, setup)
         .run();
 }
 
-
-
-fn setup(mut commands:Commands,asset_server:Res<AssetServer>){
-    
-    commands.spawn(Camera2dBundle{
-        transform : Transform::from_scale(Vec3::new(0.25,0.25,0.25)),
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    //Ajuste camera
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_scale(Vec3::new(0.25, 0.25, 0.25)),
         ..default()
     });
 
-    commands.spawn(SpriteBundle{
-     texture : asset_server.load("little_heroes.png"),
-     transform : Transform::from_xyz(0.0 ,0.0, 0.0),
-     ..default()
+    //samurai (Personagem principal)
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("little_heroes.png"),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
     });
 
+    //Ninjas (inimigos)
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("ninja.png"),
+        transform: Transform::from_xyz(-30.0, 0.0, 0.0),
+        ..default()
+    });
 }
-
-
-
