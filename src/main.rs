e909380358e8e77ed48255e0c935774c116e2aca
TalesPalma <tales.palma_2022@outlook.com>
2024-06-04 22:20:31 -0300

    Move set

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use bevy::prelude::*;
-
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_systems(Startup, setup)
+        .add_systems(Update, move_system)
         .run();
+}
+
+#[derive(Component)]
+struct Moveable {
+    speed: f32,
+}
+
+fn move_system(mut entties: Query<(&Moveable, &mut Transform)>, time: Res<Time>) {
+    for (ninja, mut transform) in entties.iter_mut() {
+        transform.translation.x += ninja.speed * time.delta_seconds();
+    }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -15,16 +26,20 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 
     //samurai (Personagem principal)
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("little_heroes.png"),
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        ..default()
-    });
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("samurai.png"),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        })
+        .insert(Moveable { speed: 1.0 });
 
     //Ninjas (inimigos)
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("ninja.png"),
-        transform: Transform::from_xyz(-30.0, 0.0, 0.0),
-        ..default()
-    });
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("ninja.png"),
+            transform: Transform::from_xyz(-30.0, 0.0, 0.0),
+            ..default()
+        })
+        .insert(Moveable { speed: 1.0 });
 }
